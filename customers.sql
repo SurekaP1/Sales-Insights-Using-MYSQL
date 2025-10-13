ffa9530de79108e7cf72375ee5e86af002cfbd86
Sureka Pararasa <166422112+SurekaP1@users.noreply.github.com>
2025-10-13 19:44:16 +0530

    Add files via upload

---
+++ customers.sql
@@ -0,0 +1,5 @@
+-- Show customer table
+SELECT * FROM sales.customers;
+
+-- show total number of customer
+SELECT count(*) FROM sales.customers;
---
+++ products.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sales.products;
+
+SELECT distinct product_code FROM sales.transactions where market_code='Mark001';
+
+SELECT * from sales.transactions where currency="USD";
+
+
